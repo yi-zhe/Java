a5d3cff33ebf09538e8a00e65e461341fa477c22
liuyang <liuyuang@coohua.com>
2017-09-19 22:03:54 +0800

    下载一个web页面

---
+++ src/com/java/net/ch5/E2SourceViewer.java
@@ -0,0 +1,33 @@
+package com.java.net.ch5;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class E2SourceViewer {
+
+    public static void main(String[] args) {
+        InputStream in = null;
+        try {
+            URL u = new URL("https://www.baidu.com");
+            in = u.openStream();
+            in = new BufferedInputStream(in);
+            Reader reader = new InputStreamReader(in);
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
