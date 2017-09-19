4474a2703869cf79ea17035f21a5d49230611c49
liuyang <liuyuang@coohua.com>
2017-09-19 15:36:55 +0800

    Local interfaces

---
+++ src/com/java/net/ch4/E8InterfaceList.java
@@ -0,0 +1,17 @@
+package com.java.net.ch4;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class E8InterfaceList {
+
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+            System.out.println(ni);
+        }
+    }
+
+}
