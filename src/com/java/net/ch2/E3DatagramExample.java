1ef64714e9553b11d0a3cc3f7954432b36524ae7
liuyang <liuyanggofurther@gmail.com>
2017-10-07 17:53:06 +0800

    DatagramPacket#getXX

---
+++ src/com/java/net/ch2/E3DatagramExample.java
@@ -0,0 +1,22 @@
+package com.java.net.ch2;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+public class E3DatagramExample {
+
+    public static void main(String[] args) {
+
+        String s = "This is a test";
+
+        try {
+            byte[] data = s.getBytes("UTF-8");
+            DatagramPacket dp = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 8090);
+            System.out.println("This packet is addressed to " + dp.getAddress() + " on port " + dp.getPort());
+            System.out.println("There are " + dp.getLength() + " bytes of data in the packet");
+            System.out.println(new String(dp.getData(), dp.getOffset(), dp.getLength(), "UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
